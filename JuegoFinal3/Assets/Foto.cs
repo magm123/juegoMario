fd12f67d6e35f09bbfc43bc13d9a54380960b1b3
maria angela <magma2592@gmail.com>
2017-06-26 22:40:25 -0500

    'juego'

---
+++ JuegoFinal3/Assets/Foto.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Foto : MonoBehaviour {
+    public string levelToLoad;
+    public float timer = 10f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Application.LoadLevel("SeleccionarAvatar");
+        }
+    }
+}
---
+++ JuegoFinal3/Assets/Principal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Principal : MonoBehaviour {
+    public string levelToLoad;
+    public float timer = 10f;
+	// Use this for initialization
+	void Start () {
+ 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if (timer <=0)
+        {
+            Application.LoadLevel("Menu");
+        }
+
+    }
+}
